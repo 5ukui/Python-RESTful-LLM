01d5aea63450aac544f5fa1bd572eee0b2bdf7e8
5ukui <m.huthaifa@proton.me>
2025-05-18 07:29:48 +0400

    Created index.html

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from typing import Union
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
-
-@app.get("/")
-async def read_root():
-    return {"Hello": "World"}
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 
 @app.get("/items/{item_id}")
